d643b7c344155088ab30c67756ea6d81ea138578
Luna Perez-Herrera <lunaperezherrera@gmail.com>
2026-06-29 15:25:28 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int lp = 0;
+        int rp = s.length() - 1;
+
+        while (lp < rp)
+        {
+            if (!isalnum(s[lp]))
+            {
+                lp++;
+                continue;
+            }
+
+            if (!isalnum(s[rp]))
+            {
+                rp--;
+                continue;
+            }
+
+            if (tolower(s[lp]) != tolower(s[rp]))
+            {
+                return false;
+            }
+
+            lp++;
+            rp--;
+        }
+
+        return true;
+    }
+};

74a7729baf34b4e5735243060df910323aa38f6c
Luna Perez-Herrera <lunaperezherrera@gmail.com>
2026-06-30 11:20:56 -0700

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int lp = 0;
+        int rp = numbers.size() - 1;
+
+        while (lp < rp)
+        {
+            int sum = numbers.at(lp) + numbers.at(rp);
+            if (sum == target)
+            {
+                vector<int> result = {lp + 1, rp + 1};
+                return result;
+            }
+
+            if (sum > target)
+            {
+                rp--;
+                continue;
+            }
+
+            if (sum < target)
+            {
+                lp++;
+                continue;
+            }
+        }
+
+        return {1, 2};
+    }
+};
